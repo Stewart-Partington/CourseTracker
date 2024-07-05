ec3d019e6ecdf0f228ea94a8a28220a340f18664
stewart-partington <tekarsolutions@gmail.com>
2024-07-05 08:39:12 -0400

    Log exceptions.

--- CourseTracker.API/Filters/ExceptionFilter.cs
+++ CourseTracker.API/Filters/ExceptionFilter.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Filters;
+using Microsoft.Extensions.Logging;
 
 namespace CourseTracker.API.Filters
 {
@@ -7,10 +8,19 @@ namespace CourseTracker.API.Filters
     public class ExceptionFilter : IExceptionFilter
     {
 
+        private readonly ILogger<ExceptionFilter> _logger;
+
+        public ExceptionFilter(ILogger<ExceptionFilter> logger)
+        {
+            _logger = logger;
+        }
+
         public void OnException (ExceptionContext context)
         {
 
             Exception exception = context.Exception;
+
+            _logger.LogError($"- From ExceptionFilter {exception.Message}", exception);
 
             context.HttpContext.Response.StatusCode = 500;
             context.Result = new JsonResult(context);
