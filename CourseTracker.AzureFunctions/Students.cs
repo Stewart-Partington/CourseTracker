f557e33fe3e71f0d44d7ea0713a7cafb4d740437
stewart-partington <tekarsolutions@gmail.com>
2024-09-13 08:41:04 -0400

    Stub out functions

--- CourseTracker.AzureFunctions/Students.cs
+++ CourseTracker.AzureFunctions/Students.cs
@@ -16,8 +16,8 @@ namespace CourseTracker.AzureFunctions
             _logger = loggerFactory.CreateLogger<Students>();
         }
 
-        [Function("Function1")]
-        public HttpResponseData Run([HttpTrigger(AuthorizationLevel.Anonymous, "get", "post")] HttpRequestData req)
+        [Function("GetSudents")]
+        public HttpResponseData GetSudents([HttpTrigger(AuthorizationLevel.Anonymous, "get", Route ="Students")] HttpRequestData req)
         {
             _logger.LogInformation("C# HTTP trigger function processed a request.");
 
@@ -27,6 +27,41 @@ namespace CourseTracker.AzureFunctions
             response.WriteString("Welcome to Azure Functions!");
 
             return response;
+        }
+
+        [Function("GetStudentById")]
+        public HttpResponseData GetStudentById([HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = "Students/{id}")] HttpRequestData req,
+            Guid id)
+        {
+
+            return null;
+
+        }
+
+        [Function("CreateStudent")]
+        public HttpResponseData CreateStudent([HttpTrigger(AuthorizationLevel.Anonymous, "post", Route = "Students")] HttpRequestData req)
+        {
+
+            return null;
+
+        }
+
+        [Function("UpdateStudent")]
+        public HttpResponseData UpdateStudent([HttpTrigger(AuthorizationLevel.Anonymous, "put", Route = "Students/{id}")] HttpRequestData req,
+            Guid id)
+        {
+
+            return null;
+
+        }
+
+        [Function("DeleteStudent")]
+        public HttpResponseData DeleteStudent([HttpTrigger(AuthorizationLevel.Anonymous, "delete", Route = "Students/{id}")] HttpRequestData req,
+            Guid id)
+        {
+
+            return null;
+
         }
 
     }
